1b23aaf9f3226f97606fa2ff669e36fce329ec92
Andrew Gray <adsgray@gmail.com>
2014-01-12 16:23:57 -0800

    Cannot use scala.util.Random?

--- src/com/adsg0186/scalatest/Util.scala
+++ src/com/adsg0186/scalatest/Util.scala
@@ -1,21 +1,21 @@
 package com.adsg0186.scalatest
 
-import scala.util.Random
 import android.widget.Button
 import android.view.View.OnClickListener
 import android.content.Context
 import android.view.View
 import android.widget.Toast
+import java.util.Random
 
 object ToastMessage {
-  lazy val rnd = new Random
+  lazy val rnd = new Random()
   val messages = List("one", "two", "three")
   def message = messages(rnd.nextInt(messages.length))
   
   def makeToastButton(b:Button, c:Context) = {
     b.setOnClickListener(new OnClickListener() {
       @Override def onClick(v:View) {
-        Toast.makeText(c, message, Toast.LENGTH_LONG).show();
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
       }
     })
     b
